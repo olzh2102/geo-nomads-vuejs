3f50b965838135f57724a34b5b17d6b559565e1a
olzhas2102 <olzhas2102@yahoo.com>
2018-05-09 23:41:20 +0200

    Navbar component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="navbar">
+    <nav class="navbar-wrapper">
+      <div class="container">
+        <a href="" class="brand-logo left">GeoNomads</a>
+        <ul class="right">
+          <li><a href="">SingUp</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style>
+.navbar-wrapper {
+  background: #9B3838;
+}
+</style>
+
+
