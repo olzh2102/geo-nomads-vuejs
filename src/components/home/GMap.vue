bc91d7d1087d0cee32fadf143e4e6bf5df4c9ca1
olzhas2102 <olzhas2102@yahoo.com>
2018-05-10 09:29:21 +0200

    Creating New Map

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div class="google-map" id="map"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'GMap',
-  data() {
-    return {
-
+  data(){
+    return{
+      lat: 52,
+      lng: 13
     }
+  },
+  methods: {
+    renderMap(){
+      const map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      });
+    }
+  },
+  mounted(){
+    this.renderMap()
   }
 }
 </script>
 
 <style>
-
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: -1;
+}
 </style>
-
