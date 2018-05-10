800ae7ca92a49f827a852009bc0d7846c044006a
olzhas2102 <olzhas2102@yahoo.com>
2018-05-10 10:15:30 +0200

    Checking if Alias exists

--- src/components/auth/Singup.vue
+++ src/components/auth/Singup.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="singup container">
-    <form class="card-panel">
+    <form @submit.prevent="singup" class="card-panel">
       <h2 class="deep-purple-text center">Singup</h2>
       <div class="field">
         <label for="email">Email: </label>
@@ -14,6 +14,7 @@
         <label for="alias">Alias: </label>
         <input type="text" name="alias" v-model="alias">
       </div>
+      <p class="red-text center" v-if="feedback">{{ feedback }}</p>
       <div class="field center">
         <button class="btn deep-purple">Singup</button>
       </div>
@@ -22,13 +23,39 @@
 </template>
 
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+
 export default {
   name: 'Singup',
   data() {
     return {
       email: null,
       password: null,
-      alias: null
+      alias: null,
+      feedback: null,
+      slug: null
+    }
+  },
+  methods: {
+    singup() {
+      if(this.alias) {
+        this.slug = slugify(this.alias, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+        let ref = db.collection('users').doc(this.slug)
+        ref.get().then(doc => {
+          if(doc.exists) {
+            this.feedback = 'This alias already exists'
+          } else {
+            this.feedback = 'This alias is free to use'
+          }
+        })
+      } else {
+        this.feedback = "Please enter an alias"
+      }
     }
   }
 }
