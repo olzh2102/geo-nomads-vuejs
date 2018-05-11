190bd9a69f3fe37fb1307694ad7446e96f5b0592
olzhas2102 <olzhas2102@yahoo.com>
2018-05-11 10:15:43 +0200

    Nomad markers on map

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -24,7 +24,27 @@ export default {
         maxZoom: 15,
         minZoom: 3,
         streetViewControl: false
-      });
+      })
+
+      db.collection('users').get().then(users => {
+        users.docs.forEach(doc => {
+          let data = doc.data()
+
+          if(data.geolocation) {
+            let marker = new google.maps.Marker({
+              position: {
+                lat: data.geolocation.lat,
+                lng: data.geolocation.lng
+              }, 
+              map
+            })
+            // add click event to marker
+            marker.addListener('click', () => {
+              console.log(doc.id)
+            })
+          }
+        })
+      })
     }
   },
   mounted(){
